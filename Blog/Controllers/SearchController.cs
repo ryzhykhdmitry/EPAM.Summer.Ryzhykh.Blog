bd04cd908f0f5584b99eb0bbb3344a78d2523b3a
Dmitry Ryzhykh <ryzhykh.dmitry@gmail.com>
2016-09-25 16:42:49 +0300

    Some UI changes, search

---
+++ Blog/Controllers/SearchController.cs
@@ -0,0 +1,46 @@
+﻿using BLL.Interface.Services;
+using Blog.Infrastructure.Mappers;
+using Blog.Models;
+using Blog.Models.ArticleViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Blog.Controllers
+{
+    public class SearchController : Controller
+    {
+        private readonly IArticleService articleService;
+        private readonly IUserService userService;
+
+        public SearchController(IArticleService articleService, IUserService userService)
+        {
+            this.articleService = articleService;
+            this.userService = userService;
+        }
+
+        [ChildActionOnly]
+        public ActionResult Index()
+        {
+            return PartialView("~/Views/Shared/_Search.cshtml");
+        }
+
+        public ActionResult Search(SearchViewModel model)
+        {
+            List<ArticleViewModel> list = new List<ArticleViewModel>();
+            var result = articleService.GetAllByPredicate(u => u.Title.Contains(model.SearchString)).
+                    Select(r => r.GetMvcEntity());
+            foreach (var article in result)
+            {
+                article.User = userService.GetById(article.AuthorId).GetMvcEntity();
+                list.Add(article);
+            }
+
+            ViewBag.QuerySearch = model.SearchString;
+
+            return View("", list);
+        }
+    }
+}
---
+++ Blog/Models/SearchViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Blog.Models
+{
+    public class SearchViewModel
+    {
+        [MinLength(2, ErrorMessage = "Too short string")]
+        public string SearchString { get; set; }
+    }
+}
